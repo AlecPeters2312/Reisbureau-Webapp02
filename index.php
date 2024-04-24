c9e00167bd59e8ad57ac53a0fb4e03b8f299d509
A <1207074@student.roc-nijmegen.nl>
2024-04-24 13:37:27 +0200

    first branch commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <link rel="stylesheet" href="css/style.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+  <title>Reisbureau</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
