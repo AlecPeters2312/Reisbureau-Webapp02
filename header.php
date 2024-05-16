ee6496a7e9ab403ef90cec18b82b1a14a53a5cbe
thijs024 <1205835@student.roc-nijmegen.nl>
2024-05-16 13:03:24 +0200

    landings pagina bijna af

---
+++ header.php
@@ -0,0 +1,23 @@
+<header>
+    <nav>
+        <div id="company-name">
+            <a href="index.php">
+                <img src="img/logo.png" alt="Deelft Blue Travel Logo">
+            </a>
+            <div id="company-tekst">
+                <p class="company-color">Blue</p>
+                <p class="company-color">Sky</p>
+                <p class="company-color">Travels</p>
+            </div>
+        </div>
+        <div class="nav-buttons">
+            <a class="white-color" href=".php">Contact</a>
+            <a class="white-color" href=".php">Over Ons</a>
+            <a class="white-color" href=".php">Registreren</a>
+            <a class="white-color" href=".php">Account</a>
+            <?php
+            include('connection.php');
+            ?>
+        </div>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <link rel="stylesheet" href="css/style.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+  <title>Reisbureau</title>
+</head>
+
+<body>
+  <?php
+  include('header.php');
+  ?>
+  <video id="background" src="img/background-vid.mp4" autoplay muted loop></video>
+  <h1 id="slogan">Discover the World in Delft's Blue Hues!</h1>
+  <h1 id="slogan">Trending Landen</h1>
+  <div class="image-space">
+    <div class="image-grid">
+      <img src="img/land1.png" alt="Land 1">
+      <img src="img/land2.png" alt="Land 2">
+      <img src="img/land3.png" alt="Land 3">
+      <img src="img/land4.png" alt="Land 4">
+      <img src="img/land5.png" alt="Land 5">
+      <img src="img/land6.png" alt="Land 6">
+      <img src="img/land7.png" alt="Land 7">
+      <img src="img/land8.png" alt="Land 8">
+      <img src="img/land9.png" alt="Land 9">
+      <img src="img/land10.png" alt="Land 10">
+      <img src="img/land11.png" alt="Land 11">
+      <img src="img/land12.png" alt="Land 12">
+      <img src="img/land13.png" alt="Land 13">
+      <img src="img/land14.png" alt="Land 14">
+      <img src="img/land15.png" alt="Land 15">
+      <img src="img/land16.png" alt="Land 16">
+      <img src="img/land17.png" alt="Land 17">
+      <img src="img/land18.png" alt="Land 18">
+      <img src="img/land19.png" alt="Land 19">
+      <img src="img/land20.png" alt="Land 20">
+      <img src="img/land21.png" alt="Land 21">
+      <img src="img/land22.png" alt="Land 22">
+      <img src="img/land23.png" alt="Land 23">
+      <img src="img/land24.png" alt="Land 24">
+    </div>
+  </div>
+  <!-- <button onclick="test()">Yo</button>
+  <form onsubmit="form()">
+    <input type="text" placeholder="voornaam" id="voornaam">
+    <input type="text" placeholder="achternaam" id="achternaam">
+    <input type="text" placeholder="straatnaam" id="straatnaam">
+    <input type="number" placeholder="huisnummer" id="huisnummer">
+    <input type="submit">
+  </form> -->
+</body>
+
+</html>
+
+<!-- <script>
+  function test() {
+    alert("Yo");
+  }
+
+  function form() {
+    var voornaam = document.getElementById("voornaam");
+    var achternaam = document.getElementById("achternaam");
+    var straatnaam = document.getElementById("straatnaam");
+    var huisnummer = document.getElementById("huisnummer");
+
+    if (voornaam.value.length == "") {
+      alert("voer je voornaam in");
+    }
+    if (achternaam.value.length == "") {
+      alert("voer je achternaam in");
+    }
+    if (straatnaam.value.length == "") {
+      alert("voer je straatnaam in");
+    }
+    if (huisnummer.value.length == "") {
+      alert("voer een getal in voor het huisnummer");
+    }
+  }
+</script> -->
