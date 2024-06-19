c25dbc802035f417bff69e27c913b5bcd19833ef
A <1207074@student.roc-nijmegen.nl>
2024-06-19 13:27:08 +0200

    getreizen voor de vijfde keer gefixt

--- getReizen.php
+++ getReizen.php
@@ -74,7 +74,7 @@ function getReizen($conn)
     <section class="reizen">
         <?php
         foreach ($reizen as $reis) {
-            ?>
+        ?>
             <div class="reisblok">
 
                 <div class="trips-square">
@@ -90,19 +90,19 @@ function getReizen($conn)
                         <input class="countries-info" type="submit" value="More Information">
                     </form>
                 </div>
-                <?php
+            <?php
         }
-}
+    }
 
 
-function updateVluchten($conn)
-{
-    $sql = "SELECT * FROM vluchten";
-    $prepare = $conn->prepare($sql);
-    $prepare->execute();
-    $vluchten = $prepare->fetchAll();
+    function updateVluchten($conn)
+    {
+        $sql = "SELECT * FROM vluchten";
+        $prepare = $conn->prepare($sql);
+        $prepare->execute();
+        $vluchten = $prepare->fetchAll();
 
-    ?>
+            ?>
 
             <?php foreach ($vluchten as $vlucht) { ?>
                 <section class="admin-center">
@@ -121,121 +121,125 @@ function updateVluchten($conn)
                         <input name="vluchtid" type="hidden" value="<?php echo $vlucht['vluchtid']; ?>">
                         <input type="submit">
                     </form>
+                </section>
+                <section class="admin-center">
                     <form action="deleteVlucht.php" method="POST">
                         <input type="hidden" name="vluchtid" value="<?php echo $vlucht['vluchtid']; ?>">
-                        <input type="submit" value="delete">
-                        <?php
+                        <input type="submit" value="Delete">
+                    </form>
+                </section>
+            <?php
             }
-}
+        }
 
-function updateReizen($conn)
-{
-    $sql = "SELECT * FROM reizen";
-    $prepare = $conn->prepare($sql);
-    $prepare->execute();
-    $reizen = $prepare->fetchAll();
-    ?>
-                    <section class="reizen">
-                        <?php
-                        foreach ($reizen as $reis) {
-                            ?>
-                            <div class="reisblok">
-                                <form method="POST" action="updateimg.php" onsubmit="showUpdate()">
-                                    <input name="afbeelding" type="text" value="<?php echo $reis['img'] ?>">
-                                    <input name="reisId" type="hidden" value="<?php echo $reis['reisid'] ?>">
-                                    <input type="submit" value="submit afbeelding">
-                                </form>
-                                <form action="updateReizen.php" method="POST" onsubmit="showUpdate()">
+        function updateReizen($conn)
+        {
+            $sql = "SELECT * FROM reizen";
+            $prepare = $conn->prepare($sql);
+            $prepare->execute();
+            $reizen = $prepare->fetchAll();
+            ?>
+            <?php
+            foreach ($reizen as $reis) {
+            ?>
+                <section class="admin-center">
+                        <form method="POST" action="updateimg.php" onsubmit="showUpdate()">
+                            <input name="afbeelding" type="text" value="<?php echo $reis['img'] ?>">
+                            <input name="reisId" type="hidden" value="<?php echo $reis['reisid'] ?>">
+                            <input type="submit" value="Submit Picture">
+                        </form>
+                </section>
+                <section class="admin-center">
+                    <form class="" action="updateReizen.php" method="POST" onsubmit="showUpdate()">
+                        <h3>
+                            <input type="text" name="reisNaam" value="<?php echo $reis['reisnaam'] ?>">
+                        </h3>
+                        <p>
+                            <select id="vluchten" name="vluchtid">
+                                <?php
+                                getVluchten($conn);
+                                ?>
+                            </select>
 
-
-                                    <div class="tekstkant">
-                                        <h3>
-                                            <input type="text" name="reisNaam" value="<?php echo $reis['reisnaam'] ?>">
-                                        </h3>
-                                        <p>
-                                            <select id="vluchten" name="vluchtid">
-                                                <?php
-                                                getVluchten($conn);
-                                                ?>
-                                            </select>
-
-                                            <textarea name="beschrijving"><?php echo $reis['beschrijving'] ?></textarea>
-                                        </p>
-                                        <p>
-                                            <input type="date" name="start-date" value="<?php echo $reis['start-date'] ?>">
-                                            € <input type="date" name="end-date" value="<?php echo $reis['end-date'] ?>">
-                                            € <input type="text" name="prijs" value="<?php echo $reis['prijs'] ?>">
-                                        </p>
-                                        <input name="reisId" type="hidden" value="<?php echo $reis['reisid'] ?>">
-                                        <input class="button" type="submit" value="submit">
-                                    </div>
-                                </form>
-                                <form action="deleteReis.php" method="POST">
-                                    <input type="hidden" name="reisid" value="<?php echo $reis['reisid']; ?>">
-                                    <input type="submit" value="delete">
-                                </form>
-                            </div>
-                            <script src="admin.js"></script>
-                            <?php
-                        }
-                        ?>
-                    </section>
-                    <?php
-}
+                            <textarea name="beschrijving"><?php echo $reis['beschrijving'] ?></textarea>
+                        </p>
+                        <p>
+                            <input type="date" name="start-date" value="<?php echo $reis['stardatum'] ?>">
+                            <input type="date" name="end-date" value="<?php echo $reis['endatum'] ?>">
+                            <input type="text" name="prijs" value="<?php echo $reis['prijs'] ?>">
+                        </p>
+                        <input name="reisId" type="hidden" value="<?php echo $reis['reisid'] ?>">
+                        <input class="button" type="submit">
+                    </form>
+                </section>
+                <section class="admin-center">
+                    <form action="deleteReis.php" method="POST">
+                        <input type="hidden" name="reisid" value="<?php echo $reis['reisid']; ?>">
+                        <input type="submit" value="Delete">
+                    </form>
+                </section>
+                <script src="admin.js"></script>
+            <?php
+            }
+            ?>
+        <?php
+        }
 
 
-function updatelocaties($conn)
-{
-    $sql = "SELECT * FROM locaties";
-    $prepare = $conn->prepare($sql);
-    $prepare->execute();
-    $locatie = $prepare->fetchAll();
+        function updatelocaties($conn)
+        {
+            $sql = "SELECT * FROM locaties";
+            $prepare = $conn->prepare($sql);
+            $prepare->execute();
+            $locatie = $prepare->fetchAll();
 
-?>
+        ?>
 
-                    <?php foreach ($locatie as $locaties) { ?>
+            <?php foreach ($locatie as $locaties) { ?>
 
-                        <section class="admin-center">
-                            <form class="reis" action="updateLocaties.php" method="POST">
-                                <input type="text" name="land" value="<?php echo $locaties["land"] ?>">
-                                <input type="text" name="stad" value="<?php echo $locaties["stad"] ?>">
-                                <input type="hidden" name="id" value="<?php echo $locaties["locatieid"] ?>">
-                                <input type="submit">
-                            </form>
-                            <form action="deleteLocatie.php" method="POST">
-                                <input type="hidden" name="lid" value="<?php echo $locaties["locatieid"] ?>">
-                                <input type="submit" value="delete">
-                            </form>
-                        </section>
-                    <?php } ?>
+                <section class="admin-center">
+                    <form class="reis" action="updateLocaties.php" method="POST">
+                        <input type="text" name="land" value="<?php echo $locaties["land"] ?>">
+                        <input type="text" name="stad" value="<?php echo $locaties["stad"] ?>">
+                        <input type="hidden" name="id" value="<?php echo $locaties["locatieid"] ?>">
+                        <input type="submit">
+                    </form>
+                </section>
+                <section class="admin-center">
+                    <form action="deleteLocatie.php" method="POST">
+                        <input type="hidden" name="lid" value="<?php echo $locaties["locatieid"] ?>">
+                        <input type="submit" value="delete">
+                    </form>
+                </section>
+            <?php } ?>
 
-                    <?php
+        <?php
 
-}
+        }
 
-function getBerichten($conn)
-{
-    $sql = "SELECT * FROM berichten";
-    $prepare = $conn->prepare($sql);
-    $prepare->execute();
-    $berichten = $prepare->fetchAll();
+        function getBerichten($conn)
+        {
+            $sql = "SELECT * FROM berichten";
+            $prepare = $conn->prepare($sql);
+            $prepare->execute();
+            $berichten = $prepare->fetchAll();
 
-    ?>
+        ?>
 
 
-                    <?php foreach ($berichten as $bericht) { ?>
+            <?php foreach ($berichten as $bericht) { ?>
 
-                        <section class="admin-center">
-                            <h1>Berichten</h1>
-                            <h2>Email: <?php echo $bericht["email"] ?></h2>
-                            <h3>Bericht: <?php echo $bericht["bericht"] ?></h3>
-                            <form action="delete-mes.php" method="POST">
-                                <input type="hidden" name="berichtid" value="<?php echo $bericht["berichtid"] ?>">
-                                <input type="submit" value="delete">
-                            </form>
-                        </section>
-                    <?php } ?>
+                <section class="admin-center">
+                    <h1>Berichten</h1>
+                    <h2>Email: <?php echo $bericht["email"] ?></h2>
+                    <h3>Bericht: <?php echo $bericht["bericht"] ?></h3>
+                    <form action="delete-mes.php" method="POST">
+                        <input type="hidden" name="berichtid" value="<?php echo $bericht["berichtid"] ?>">
+                        <input type="submit" value="delete">
+                    </form>
+                </section>
+            <?php } ?>
 
-                    <?php
+        <?php
 
-}
+        }
